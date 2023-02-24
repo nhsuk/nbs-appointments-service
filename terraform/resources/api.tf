5d32a87726e55fc8b2b321ea3ab5c0762fbc219e
Vincent Crowe <vincent.crowe1@nhs.net>
2023-02-24 16:21:17 +0000

    pivot to linux web app containers

--- terraform/resources/api.tf
+++ terraform/resources/api.tf
@@ -1,6 +1,6 @@
-﻿data "azurerm_function_app_host_keys" "nbs_appts_funcapp" {
-  name                = azurerm_function_app.nbs_appts_funcapp.name
-  resource_group_name = azurerm_resource_group.nbs_appts_rg.name
+﻿data "azurerm_container_registry" "container_registry" {
+  resource_group_name = var.registry_group
+  name                = var.registry_name
 }
 
 resource "azurerm_resource_group" "nbs_appts_rg" {
@@ -8,6 +8,52 @@ resource "azurerm_resource_group" "nbs_appts_rg" {
   location = "${var.location}"
   tags = local.allTags
 }
+
+resource "azurerm_service_plan" "nbs_appts_sp" {
+  name                = "${var.application}-sp-${var.environment}-${var.loc}"
+  resource_group_name = azurerm_resource_group.nbs_appts_rg.name
+  location            = azurerm_resource_group.nbs_appts_rg.location
+  os_type             = "Linux"
+  sku_name            = var.sku_name
+  tags = local.allTags
+}
+
+resource "azurerm_linux_web_app" "nbs_appts_app" {
+  name                    = "${var.application}-app-${var.environment}-${var.loc}"
+  resource_group_name     = azurerm_resource_group.nbs_appts_rg.name
+  location                = azurerm_resource_group.nbs_appts_rg.location
+  service_plan_id         = azurerm_service_plan.nbs_appts_sp.id
+  https_only              = true
+
+  site_config {    
+    minimum_tls_version = "1.2"
+    container_registry_use_managed_identity = true
+    application_stack {
+      docker_image     = "${data.azurerm_container_registry.container_registry.login_server}/${var.docker_image}"
+      docker_image_tag = "${var.docker_image_tag}"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      app_settings,
+      tags # Ignore changes to tags, avoiding loss of Azure link to App Insights 
+    ]
+  }
+}
+
+resource "azurerm_role_assignment" "acrpull_role" {
+  scope                = data.azurerm_container_registry.container_registry.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.nbs_appts_app.identity.0.principal_id
+}
+
+
+
 
 resource "azurerm_storage_account" "nbs_appts_stacc" {
   name                     = "${var.application_short}stacc${var.environment}${var.loc}"
