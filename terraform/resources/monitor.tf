867c6aa6d7cf84a7bc6a6e0fde06e72e478d820d
Kim Crowe <kim.crowe@airelogic.com>
2023-05-16 15:09:32 +0100

    added emails to monitor

--- terraform/resources/monitor.tf
+++ terraform/resources/monitor.tf
@@ -18,10 +18,40 @@ resource "azurerm_monitor_action_group" "nbs_appts_app_alert_action_group" {
   resource_group_name = azurerm_resource_group.nbs_appts_rg.name
   short_name          = "${var.environment}-${var.loc}"
 
-   email_receiver {
-    name          = "Team Jabby Member"
-    email_address = "kim.crowe4@nhs.net; vincent.crowe1@nhs.net; paul.lewis43@nhs.net"
+  email_receiver {
+    name          = "Kim Crowe"
+    email_address = "kim.crowe4@nhs.net"
   }
+
+  email_receiver {
+    name          = "Vincent Crowe"
+    email_address = "vincent.crowe1@nhs.net"
+  }
+
+  email_receiver {
+    name          = "Paul Lewis"
+    email_address = "paul.lewis43@nhs.net"
+  }
+
+  email_receiver {
+    name          = "Usman Iqbal"
+    email_address = "usman.iqbal6@nhs.net"
+  }
+
+  email_receiver {
+    name          = "Anton Forbes"
+    email_address = "anton.forbes1@nhs.net"
+  }
+
+	email_receiver {
+    name          = "Darren Lightfoot"
+    email_address = "darren.lightfoot1@nhs.net"
+  }
+
+	email_receiver {
+    name          = "Leon Bamforth"
+    email_address = "leon.bamforth2@nhs.net"
+  }  
 }
 
 resource "azurerm_monitor_metric_alert" "nbs_appts_app_http_401_alert" {
