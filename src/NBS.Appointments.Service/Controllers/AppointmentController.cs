e2ee32be42d32337bd70381b377360b8119f6227
Stephen Banks <stephen.banks@airelogic.com>
2023-05-31 10:29:28 +0100

    Moving filtering appointments logic into helper class and adding tests

--- src/NBS.Appointments.Service/Controllers/AppointmentController.cs
+++ src/NBS.Appointments.Service/Controllers/AppointmentController.cs
@@ -3,6 +3,7 @@ using NBS.Appointments.Service.Core.Dtos.Qflow.Descriptors;
 using NBS.Appointments.Service.Core.Helpers;
 using NBS.Appointments.Service.Core.Interfaces.Services;
 using NBS.Appointments.Service.Extensions;
+using NBS.Appointments.Service.Helpers;
 using NBS.Appointments.Service.Models;
 using NBS.Appointments.Service.Validators;
 using System;
@@ -123,9 +124,7 @@ namespace NBS.Appointments.Service.Controllers
 
             var appointments = await _qflowService.GetAllCustomerAppointments(qflowCustomer.ResponseData.Id);
 
-            if (!includePastAppointments)
-                appointments = appointments.Where(x => DateTime.Compare(x.AppointmentDate.ToUniversalTime(), DateTime.Today.ToUniversalTime()) >= 0).ToList();
-
+            appointments = AppointmentsHelper.FilterPastCustomerAppointments(appointments, includePastAppointments);
             var customerName = $"{qflowCustomer.ResponseData.FirstName} {qflowCustomer.ResponseData.LastName}";
 
             return Ok(GetAppointmentsResponse.FromQflowResponse(appointments, nhsNumber, customerName));
