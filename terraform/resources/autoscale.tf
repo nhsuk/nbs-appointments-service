9304504f4b37ca834bcc33fb55a1388935841e1c
Paul Lewis <paul.lewis@nhschoices.net>
2023-05-31 13:31:24 +0100

    added force delete

--- terraform/resources/autoscale.tf
+++ terraform/resources/autoscale.tf
@@ -1,4 +1,4 @@
-resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale" {
+resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale2" {
   #count               = var.enable_autoscaling ? 1 : 0   
   #name                = "${var.application_short}${var.environment}${var.loc}"
   name                = "myAutoscaleSetting"
@@ -8,7 +8,7 @@ resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale" {
   target_resource_id  = azurerm_service_plan.nbs_appts_sp.id
   lifecycle {
     ignore_changes = [force_delete]
-
+  }
   profile {
     name = "default"
 
@@ -60,51 +60,3 @@ resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale" {
   }
 }
 
-/*
-{
-    "location": "UK South",
-    "tags": {},
-    "properties": {
-        "name": "nbs-appts-sp-perf-uks-Autoscale-152",
-        "enabled": false,
-        "targetResourceUri": "/subscriptions/07748954-52d6-46ce-95e6-2701bfc715b4/resourceGroups/nbs-appts-rg-perf-uks/providers/Microsoft.Web/serverfarms/nbs-appts-sp-perf-uks",
-        "profiles": [
-            {
-                "name": "Auto created default scale condition",
-                "capacity": {
-                    "minimum": "1",
-                    "maximum": "10",
-                    "default": "1"
-                },
-                "rules": [
-                    {
-                        "scaleAction": {
-                            "direction": "Increase",
-                            "type": "ChangeCount",
-                            "value": "1",
-                            "cooldown": "PT5M"
-                        },
-                        "metricTrigger": {
-                            "metricName": "CpuPercentage",
-                            "metricNamespace": "microsoft.web/serverfarms",
-                            "metricResourceUri": "/subscriptions/07748954-52d6-46ce-95e6-2701bfc715b4/resourceGroups/nbs-appts-rg-perf-uks/providers/Microsoft.Web/serverFarms/nbs-appts-sp-perf-uks",
-                            "operator": "GreaterThan",
-                            "statistic": "Average",
-                            "threshold": 70,
-                            "timeAggregation": "Average",
-                            "timeGrain": "PT1M",
-                            "timeWindow": "PT10M",
-                            "Dimensions": [],
-                            "dividePerInstance": false
-                        }
-                    }
-                ]
-            }
-        ],
-        "notifications": [],
-        "targetResourceLocation": "UK South"
-    },
-    "id": "/subscriptions/07748954-52d6-46ce-95e6-2701bfc715b4/resourceGroups/nbs-appts-rg-perf-uks/providers/microsoft.insights/autoscalesettings/nbs-appts-sp-perf-uks-Autoscale-152",
-    "name": "nbs-appts-sp-perf-uks-Autoscale-152",
-    "type": "Microsoft.Insights/autoscaleSettings"
-}*/
