31436b061f4d54354f80c088a21a3d5d87e90ebf
Paul Lewis <paul.lewis@nhschoices.net>
2023-05-31 14:04:52 +0100

    returned to current config

--- terraform/resources/autoscale.tf
+++ terraform/resources/autoscale.tf
@@ -1,7 +1,6 @@
-resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale3" {
-  #count               = var.enable_autoscaling ? 1 : 0   
-  #name                = "${var.application_short}${var.environment}${var.loc}"
-  name                = "myAutoscaleSetting2"
+resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale" {
+  count               = var.enable_autoscaling ? 1 : 0   
+  name                = "${var.application_short}${var.environment}${var.loc}"
   enabled             = true
   resource_group_name = azurerm_resource_group.nbs_appts_rg.name
   location            = azurerm_resource_group.nbs_appts_rg.location
@@ -9,7 +8,6 @@ resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale3" {
 
   profile {
     name = "default"
-
     capacity {
       default = 1
       minimum = 1
@@ -56,6 +54,5 @@ resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale3" {
       }
     }
   }
-
 }
 
