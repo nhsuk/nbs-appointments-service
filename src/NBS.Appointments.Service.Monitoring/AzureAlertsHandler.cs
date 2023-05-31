4d9b3a6335217347a5ef433d5c94661a1e39851d
Kim Crowe <kim.crowe@airelogic.com>
2023-05-31 13:43:16 +0100

    added slack webhook url config

--- src/NBS.Appointments.Service.Monitoring/AzureAlertsHandler.cs
+++ src/NBS.Appointments.Service.Monitoring/AzureAlertsHandler.cs
@@ -9,82 +9,93 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
-namespace NBS.Appointments.Service.Monitoring
+namespace NBS.Appointments.Service.Monitoring;
+
+public class AzureAlertsHandler
 {
-    public static partial class AzureAlertsHandler
+    private readonly Options _options;
+    public AzureAlertsHandler(IOptions<Options> options)
     {
-        [FunctionName("AzureAlertsHandler")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
-            HttpRequest alertRequest,
-            ILogger log)
+        _options = options.Value;
+    }
+
+    [FunctionName("AzureAlertsHandler")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        HttpRequest alertRequest,
+        ILogger log)
+    {
+        var requestBody = await new StreamReader(alertRequest.Body).ReadToEndAsync();
+
+        var alertData = JsonSerializer.Deserialize<AlertData>(requestBody);
+
+        if (alertData.Data.Essentials.MonitorCondition == "Fired")
         {
-            var requestBody = await new StreamReader(alertRequest.Body).ReadToEndAsync();
-
-            var alertData = JsonSerializer.Deserialize<AlertData>(requestBody);
-
-            if (alertData.Data.Essentials.MonitorCondition == "Fired")
+            var alertName = alertData.Data.Essentials.AlertRule;
+            var alertTarget = alertData.Data.Essentials.AlertTargetIDs[0];
+            var alertTargetResourceName = alertTarget.Split("/").Last();
+            var metric = alertData.Data.AlertContext.Condition.ConditionDetails[0].MetricName;
+            var alertOperator = alertData.Data.AlertContext.Condition.ConditionDetails[0].Operator;
+            var threshold = alertData.Data.AlertContext.Condition.ConditionDetails[0].Threshold;
+            var notification = new
             {
-                var alertName = alertData.Data.Essentials.AlertRule;
-                var alertTarget = alertData.Data.Essentials.AlertTargetIDs[0];
-                var alertTargetResourceName = alertTarget.Split("/").Last();
-                var metric = alertData.Data.AlertContext.Condition.ConditionDetails[0].MetricName;
-                var alertOperator = alertData.Data.AlertContext.Condition.ConditionDetails[0].Operator;
-                var threshold = alertData.Data.AlertContext.Condition.ConditionDetails[0].Threshold;
-                var notification = new
+                blocks = new[]
                 {
-                    blocks = new[]
+                    new
                     {
-                        new
+                        type = "section",
+                        text = new
                         {
-                            type = "section",
-                            text = new
-                            {
-                                type = "mrkdwn",
-                                text = ":alert: *Alert fired* :alert:"
-                            }
-                        },
-                        new
+                            type = "mrkdwn",
+                            text = ":alert: *Alert fired* :alert:"
+                        }
+                    },
+                    new
+                    {
+                        type = "section",
+                        text = new
                         {
-                            type = "section",
-                            text = new
-                            {
-                                type = "mrkdwn",
-                                text = $"*{alertName} has fired*! \n\n_{metric} {alertOperator} {threshold}_ "
-                            }
-                        },
-                        new
+                            type = "mrkdwn",
+                            text = $"*{alertName} has fired*! \n\n_{metric} {alertOperator} {threshold}_ "
+                        }
+                    },
+                    new
+                    {
+                        type = "section",
+                        text = new
                         {
-                            type = "section",
-                            text = new
-                            {
-                                type = "mrkdwn",
-                                text =
-                                    $"View all alerts for {alertTargetResourceName} <https://portal.azure.com/#@nhschoices.net/resource{alertTarget}/alerts|here>"
-                            }
+                            type = "mrkdwn",
+                            text =
+                                $"View all alerts for {alertTargetResourceName} <https://portal.azure.com/#@nhschoices.net/resource{alertTarget}/alerts|here>"
                         }
                     }
-                };
+                }
+            };
 
-                try
-                {
-                    await SendSlackNotification(notification);
-                }
-                catch (Exception ex)
-                {
-                    System.Console.WriteLine(ex);
-                }
+            try
+            {
+                await SendSlackNotification(notification);
             }
-
-            return new OkResult();
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
         }
 
-        private static Task SendSlackNotification(object notification)
-        {
-            var client = new HttpClient();
-            var uri = "https://requestinspector.com/inspect/01h1p7ztdc9we8vdw50h9gsz6e";
-            return client.PostAsJsonAsync(uri, notification);
-        }
+        return new OkResult();
+    }
+
+    private Task SendSlackNotification(object notification)
+    {
+        var client = new HttpClient();
+        var uri = _options.SlackWebhookUrl;
+        return client.PostAsJsonAsync(uri, notification);
+    }
+
+    public class Options
+    {
+        public string SlackWebhookUrl { get; set; }
     }
 }
