883e2636700d1550a700ad12fc0452686d499c03
Paul Lewis <paul.lewis@nhschoices.net>
2023-06-01 08:38:00 +0100

    removed enabled

--- terraform/resources/autoscale.tf
+++ terraform/resources/autoscale.tf
@@ -1,7 +1,6 @@
 resource "azurerm_monitor_autoscale_setting" "nbs_appts_sp_autoscale" {
-  count               = var.enable_autoscaling ? 1 : 0   
+  count               = var.enable_autoscaling ? 1 : 0
   name                = "${var.application_short}${var.environment}${var.loc}"
-  enabled             = true
   resource_group_name = azurerm_resource_group.nbs_appts_rg.name
   location            = azurerm_resource_group.nbs_appts_rg.location
   target_resource_id  = azurerm_service_plan.nbs_appts_sp.id
