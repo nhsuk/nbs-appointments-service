244243bd17735e2067925b065e9611e48f4eb0ab
Kim Crowe <kim.crowe@airelogic.com>
2023-06-14 15:56:52 +0100

    Added missing file from merge

---
+++ terraform/environments/stag/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.58.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "covid19-booking-rg-tfstate-stag-uks"
+    storage_account_name = "covidbookstagtf"
+    container_name       = "tfstate"
+    key                  = "nbs-appts-stag.tfstate"
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "docker_image_tag" {
+  type = string
+}
+
+module "api" {
+  source             = "../../resources"
+  location           = "uksouth"
+  environment        = "stag"
+  loc                = "uks"
+  sku_name           = "S1"
+  enable_autoscaling = true
+  docker_image_tag   = var.docker_image_tag
+}
+
+
