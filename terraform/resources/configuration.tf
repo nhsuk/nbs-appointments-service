793d1f22999de42337c83268fc5e0244aaf34618
Kim Crowe <kim.crowe@airelogic.com>
2023-06-15 16:14:56 +0100

    changed access policy

--- terraform/resources/configuration.tf
+++ terraform/resources/configuration.tf
@@ -14,23 +14,22 @@ resource "azurerm_key_vault" "nbs_appts_kv" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
-}
 
-resource "azurerm_key_vault_access_policy" "key_vault_access_policy_serviceprinciple" {
-  key_vault_id = azurerm_key_vault.nbs_appts_kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
-  secret_permissions = [
-    "Backup",
-    "Delete",
-    "Get",
-    "List",
-    "Purge",
-    "Recover",
-    "Restore",
-    "Set",
-  ]
+    secret_permissions = [
+      "Backup",
+      "Delete",
+      "Get",
+      "List",
+      "Purge",
+      "Recover",
+      "Restore",
+      "Set",
+    ]
+  }
 }
 
 resource "azurerm_key_vault_access_policy" "key_vault_access_policy_alerthandler_func" {
