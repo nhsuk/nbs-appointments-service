51f9683b3028dd99f8940b0c5b824c7d96e27b28
Kim Crowe <kim.crowe@airelogic.com>
2023-08-11 16:36:34 +0100

    updated pipeline scripts

--- releasePipelineScripts/role-assignments.ps1
+++ releasePipelineScripts/role-assignments.ps1
@@ -1,15 +1,16 @@
 Param(
-    [string] [Parameter(Mandatory=$true)] $subscription,
-    [string] [Parameter(Mandatory=$true)] $assigneeId
+    [string] [Parameter(Mandatory=$true)] $subscriptionId,
+    [string] [Parameter(Mandatory=$true)] $assigneeId,
+    [string] [Parameter(Mandatory=$true)] $environment
 )
 
 $RoleAssignments = @(
-    @{role="User Access Administrator"; rg="nbs-appts-rg-exp-uks"}
+    @{role="User Access Administrator"; rg="nbs-appts-rg-$environment-uks"}
     @{role="Reader"; rg="covid19-booking-rg-dev-uks"}
     @{role="User Access Administrator"; rg="covid19-booking-rg-dev-uks"}
 )
 
 $RoleAssignments | ForEach-Object {
-    $resourceGroupId = -join ("/subscriptions/$subscription/resourceGroups/", $_.rg)
+    $resourceGroupId = -join ("/subscriptions/$subscriptionId/resourceGroups/", $_.rg)
     az role assignment create --assignee $assigneeId --role $_.role --scope $resourceGroupId
 }
